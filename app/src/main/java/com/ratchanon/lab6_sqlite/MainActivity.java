e9b140dce95d714e03c7a9a26e1d8c8dbef375bb
Gkung <getjung16@gmail.com>
2025-02-22 20:31:45 +0700

    Update

--- app/src/main/java/com/ratchanon/lab6_sqlite/MainActivity.java
+++ app/src/main/java/com/ratchanon/lab6_sqlite/MainActivity.java
@@ -1,15 +1,27 @@
 package com.ratchanon.lab6_sqlite;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    FrameLayout frameLayout;
+    BottomNavigationView bottomNavigation;
+    Fragment selectedFragment = null;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +32,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+        bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        loadFragment(new ViewFragment());
+
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            Fragment tempSelectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.item_1) {
+                tempSelectedFragment = new ViewFragment();
+            }else if(itemId == R.id.item_2) {
+                tempSelectedFragment = new AddFragment();
+            }
+
+            if (selectedFragment != null && Objects.equals(tempSelectedFragment, selectedFragment)) return true;
+
+            if (tempSelectedFragment != null) {
+                loadFragment(tempSelectedFragment);
+            }
+            return true;
+        });
+    }
+    // Method to load a fragment into the container
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, fragment)
+                .commit();
     }
 }
